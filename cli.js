6e69e25f09454348981527f3691931e3ae63095e
NatThome <nathythome05@gmail.com>
2019-12-02 14:31:00 -0300

    print sem [{}]

--- cli.js
+++ cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
+const chalk = require('chalk');
 const mdLinks = require('./lib/index.js');
 
 const path = process.argv.slice(2).pop();
-
 mdLinks(path)
-  .then(result => console.log(result))
+  .then((result) => result.forEach(element => {
+    console.log('\n', chalk.yellow(element.href),'\n', element.text.substring(0, 50));
+  }))
   .catch(error => console.log(error));
+
