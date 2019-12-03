0005eeb53f5e988cf99a57d42526d4035ddeb78d
NatThome <nathythome05@gmail.com>
2019-12-03 17:36:33 -0300

    testes ok

--- cli.js
+++ cli.js
@@ -1,12 +1,13 @@
-#!/usr/bin/env node
+/* eslint-disable comma-spacing */
 /* eslint-disable no-console */
+// #!/usr/bin/env node
+
 const chalk = require('chalk');
 const mdLinks = require('./lib/index.js');
 
 const path = process.argv.slice(2).pop();
 mdLinks(path)
-  .then((result) => result.forEach(element => {
+  .then(result => result.forEach((element) => {
     console.log('\n', chalk.yellow(element.href),'\n', element.text.substring(0, 50));
   }))
   .catch(error => console.log(error));
-
